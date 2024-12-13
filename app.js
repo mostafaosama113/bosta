320a89f7159c7248490f8ba9c5dd75f38765550c
mostafa osama <hitmanx97@gmail.com>
2024-12-13 23:54:30 +0200

    rate limiter

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const borrowerRouter = require('./routes/borrowerRoutes')
 const bookRouter = require('./routes/bookRouts')
 const borrowingRouter = require('./routes/borrowingRouts')
 const {authenticator} = require("./middlewares/authenticatorMiddleware");
+const {rateLimiter} = require("./middlewares/rateLimiterMiddleware");
 const app = express()
 app.use(express.json())
+app.use(rateLimiter);
 app.use(authenticator);
 app.use('/api/borrower' , borrowerRouter)
 app.use('/api/book' , bookRouter)
