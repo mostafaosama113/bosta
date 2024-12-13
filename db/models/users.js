3a1259345e389a72c517b33954655349141ab73d
mostafa osama <hitmanx97@gmail.com>
2024-12-13 13:29:25 +0200

    bcrypt the password

--- db/models/users.js
+++ db/models/users.js
@@ -1,45 +1,56 @@
 'use strict';
 const {
   Model,
-  Sequelize
+  Sequelize,
+  DataTypes
 } = require('sequelize');
 const sequelize = require('../../config/database')
+const bcrypt = require('bcrypt')
 module.exports = sequelize.define('users' , 
   {
     id: {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-      type: Sequelize.INTEGER
+      type: DataTypes.INTEGER
     },
     firstName: {
-      type: Sequelize.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     lastName: {
-      type: Sequelize.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-      type: Sequelize.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     registeredDate: {
-      type: Sequelize.DATE
+      type: DataTypes.DATE
     },
     password: {
-      type: Sequelize.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value){
+        const hashPassword = bcrypt.hashSync(value , 12)
+        this.setDataValue('password' , hashPassword)
+      }
     },
     usertype: {
-      type: Sequelize.ENUM('0' , '1') // 0 for Borrowers , 1 for Manger
+      type: DataTypes.ENUM('0' , '1') // 0 for Borrowers , 1 for Manger
     },
     createdAt: {
       allowNull: false,
-      type: Sequelize.DATE
+      type: DataTypes.DATE
     },
     updatedAt: {
       allowNull: false,
-      type: Sequelize.DATE
+      type: DataTypes.DATE
     },
     deletedAt:{
-      type: Sequelize.DATE
+      type: DataTypes.DATE
     }
   } , {
     paranoid : true, // for soft-delete
