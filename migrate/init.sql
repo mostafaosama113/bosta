171252858344e0e93bc5bd42bd3a6b5c4cd9071d
mostafa osama <hitmanx97@gmail.com>
2024-12-14 20:10:55 +0200

    add dummy data

--- migrate/init.sql
+++ migrate/init.sql
@@ -29,3 +29,22 @@ CREATE TABLE IF NOT EXISTS Borrowing_Process (
     FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE,
     is_deleted BOOLEAN DEFAULT FALSE 
 );
+
+
+INSERT INTO borrowers (first_name, last_name, email, password) 
+VALUES 
+('John', 'Doe', 'john.doe@example.com', 'password123'),
+('Jane', 'Smith', 'jane.smith@example.com', 'password456'),
+('Alice', 'Johnson', 'alice.johnson@example.com', 'password789');
+
+INSERT INTO Books (title, author, isbn, available_quantity, shelf_location) 
+VALUES 
+('The Great Gatsby', 'F. Scott Fitzgerald', '9780743273565', 5, 'A1'),
+('1984', 'George Orwell', '9780451524935', 10, 'B2'),
+('To Kill a Mockingbird', 'Harper Lee', '9780061120084', 3, 'C3');
+
+INSERT INTO Borrowing_Process (borrower_id, book_id, due_date, return_date) 
+VALUES 
+(1, 1, '2024-12-21', NULL),
+(2, 2, '2024-12-15', '2024-12-10'),
+(3, 3, '2024-12-20', NULL);
